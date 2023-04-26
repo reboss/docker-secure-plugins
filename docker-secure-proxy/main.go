d1267c6aac4566f26117b1f541fe785be412df65
John Robbins <j.reboss@gmail.com>
2023-04-25 19:34:30 -0600

    Proxy listens for cli requests and forwards them to docker.sock
    I'm getting a 400 bad request error from the Daemon now, so I will have
    to inspect the packets in wireshark or something to determine what the
    issue is

--- docker-secure-proxy/main.go
+++ docker-secure-proxy/main.go
@@ -3,6 +3,9 @@ package main
 import (
     "fmt"
     "bytes"
+    "bufio"
+    "io"
+    "net"
     "encoding/json"
     "io/ioutil"
     "log"
@@ -78,7 +81,7 @@ func proxyHandler(w http.ResponseWriter, req *http.Request) {
     var reqBytes []byte
     var newReq *http.Request
     var res *http.Response
-    var resBytes []byte
+    var conn net.Conn
 
     reqBytes, err = json.Marshal(body)
     if err != nil {
@@ -90,22 +93,45 @@ func proxyHandler(w http.ResponseWriter, req *http.Request) {
         goto internalServerError
     }
 
-    res, err = http.DefaultClient.Do(newReq)
+    // Dial the Unix domain socket
+    conn, err = net.Dial("unix", "/var/run/docker.sock")
     if err != nil {
-        http.Error(w, err.Error(), http.StatusBadGateway)
-        return
+        goto internalServerError
+    }
+    defer conn.Close()
+
+    // Forward the incoming request to the Unix domain socket
+    err = newReq.Write(conn)
+    if err != nil {
+        goto internalServerError
+    }
+
+    // Read the response from the Unix domain socket
+    res, err = http.ReadResponse(bufio.NewReader(conn), req)
+    if err != nil {
+        goto internalServerError
     }
     defer res.Body.Close()
 
-    resBytes, err = getForwardRes(res, w)
-    if err != nil {
-        goto internalServerError
+    fmt.Println(req)
+    fmt.Println(res)
+
+    // Copy the response headers to the proxy response
+    for key, values := range res.Header {
+        for _, value := range values {
+            w.Header().Add(key, value)
+        }
     }
 
-    _, err = w.Write(resBytes)
+    // Copy the response status code to the proxy response
+    w.WriteHeader(res.StatusCode)
+
+    // Copy the response body to the proxy response
+    _, err = io.Copy(w, res.Body)
     if err != nil {
         goto internalServerError
     }
+    return
 
 internalServerError:
     http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -114,6 +140,16 @@ internalServerError:
 
 func main() {
     http.HandleFunc("/", proxyHandler)
-    fmt.Println("Listening on port 8080...")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    //socket := "/var/run/docker.sock"
+    socket := "/tmp/proxy.sock"
+    listener, err := net.Listen("unix", socket)
+    if err != nil {
+        log.Fatal("Error listening on socket %s: %v", socket, err)
+        return
+    }
+    defer listener.Close()
+
+    fmt.Println("Listening on %s", socket)
+
+    log.Fatal(http.Serve(listener, nil))
 }
